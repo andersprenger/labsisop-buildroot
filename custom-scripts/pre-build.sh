ae7f7a251dabf3aa8ee49690c376d6a40e6091a3
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2025-04-09 13:03:55 +0000

    add classroom files

---
+++ custom-scripts/pre-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+if [-d $BASE_DIR]; then
+    cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+    chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+fi
